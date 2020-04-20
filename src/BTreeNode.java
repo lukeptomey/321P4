c2a5a66d33380778bac35ef04eac286ccbec0504
lukeptomey <lukeptomey@u.boisestate.edu>
2020-04-20 12:33:45 -0600

    Beginning of BTree Constructor

--- src/BTreeNode.java
+++ src/BTreeNode.java
@@ -1,4 +1,14 @@
 
 public class BTreeNode {
-
+	String[] keys; // An array of keys 
+    int t; // Minimum degree 
+    BTreeNode[] C; // An array of child pointers 
+    int n; // Current number of keys 
+    boolean leaf; // Whether node is leaf
+   // numbers
+   // pointer to data
+	BTreeNode(int t, boolean leaf ){
+		
+	}
+	
 }
