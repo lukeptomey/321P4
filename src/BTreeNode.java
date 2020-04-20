9142c3fcf898517d9aa4bab7aaf335d05b8a6954
lukeptomey <lukeptomey@u.boisestate.edu>
2020-04-20 12:45:05 -0600

    Edit of parameters

--- src/BTreeNode.java
+++ src/BTreeNode.java
@@ -1,13 +1,15 @@
 
 public class BTreeNode {
-	String[] keys; // An array of keys 
+	String[] keys; // An array of key values
     int t; // Minimum degree 
     BTreeNode[] C; // An array of child pointers 
     int n; // Current number of keys 
     boolean leaf; // Whether node is leaf
-   // numbers
-   // pointer to data
-	BTreeNode(int t, boolean leaf ){
+    BTreeNode parent;// Parent Node
+
+	BTreeNode(int t, BTreeNode parent ){
+		this.t=t;
+		this.parent= parent;
 		
 	}
 	
