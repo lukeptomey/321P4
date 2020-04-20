ea8fac773d5b5ef096a57a70b2e1499e5e6aa67d
lukeptomey <55600217+lukeptomey@users.noreply.github.com>
2020-04-20 13:24:23 -0600

    Added methods for node class.

--- src/BTreeNode.java
+++ src/BTreeNode.java
@@ -2,7 +2,7 @@
 public class BTreeNode {
 	String[] keys; // An array of key values
     int t; // Minimum degree 
-    BTreeNode[] C; // An array of child pointers 
+    BTreeNode children[] ; // An array of child pointers 
     int n; // Current number of keys 
     boolean leaf; // Whether node is leaf
     BTreeNode parent;// Parent Node
@@ -11,6 +11,23 @@ public class BTreeNode {
 		this.t=t;
 		this.parent= parent;
         keys = new String[2*t - 1];
-	}
+        children = new BTreeNode [2*t];
+        leaf = true; //Every node starts as leaf
+        n =0;
+
+    }
+
+    public String getValue (int index){
+        return keys [index];
+    }
+
+    public BTreeNode getChild(int index){
+        return children[index];
+    }
+    public void setIfLeaf(boolean statement){
+        leaf=statement;
+    }
+
+    
 	
 }
