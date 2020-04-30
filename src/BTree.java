e740c9370cf8563ae80663d9648f420b5ea254db
lukeptomey <lukeptomey@u.boisestate.edu>
2020-04-30 12:03:40 -0600

    Creation of BTree Variables and comments

--- src/BTree.java
+++ src/BTree.java
@@ -1,4 +1,10 @@
 
 public class BTree {
+    int order;
+    BTreeNode root;
 
+public BTree(int order){
+    this.order=order;
+    root = new BTreeNode(order,null);
+    }
 }
--- src/BTreeNode.java
+++ src/BTreeNode.java
@@ -1,4 +1,8 @@
-
+/**
+ * BTree Node
+ * @author Luke Ptomey
+ * BTree Node object that will make up BTree
+ */
 public class BTreeNode {
 	String[] keys; // An array of key values
     int t; // Minimum degree 
@@ -10,24 +14,37 @@ public class BTreeNode {
 	BTreeNode(int t, BTreeNode parent ){
 		this.t=t;
 		this.parent= parent;
-        keys = new String[2*t - 1];
-        children = new BTreeNode [2*t];
+        keys = new String[2*t - 1]; //key values
+        children = new BTreeNode [2*t]; //references
         leaf = true; //Every node starts as leaf
         n =0;
 
     }
-
+/**
+ * Gets value of key
+ * @param index
+ * @return key value
+ */
     public String getValue (int index){
         return keys [index];
     }
-
+    /**
+     * Gets child node at given index
+     * @param index
+     * @return child node
+     */
     public BTreeNode getChild(int index){
         return children[index];
     }
+    /**
+     * Sets status if node is a leaf
+     * @param statement
+     */
     public void setIfLeaf(boolean statement){
         leaf=statement;
     }
-cvbcbcb
+
+
     
 	
 }
