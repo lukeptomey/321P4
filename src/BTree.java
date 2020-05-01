089bd0823195d4a28bfb329a81409739fb3a15ac
lukeptomey <lukeptomey@u.boisestate.edu>
2020-04-30 23:31:15 -0600

    Beginning of Insert Method

--- src/BTree.java
+++ src/BTree.java
@@ -1,4 +1,8 @@
-
+/**
+ * BTree
+ * @author Luke Ptomey
+ * BTree that holds BTreeObjects
+ */
 public class BTree {
     int order;
     BTreeNode root;
@@ -7,4 +11,20 @@ public BTree(int order){
     this.order=order;
     root = new BTreeNode(order);
     }
+    /**
+     * Inserts new node
+     */
+    public void insert(BTree tree, int keys){
+    BTreeNode start = tree.root;
+    if(start.n == 2 *order-1){
+       BTreeNode s= new BTreeNode(order);
+         }
+    }
+    /**
+     * Splits node if full
+     */
+    public void split(){
+
+    }
+
 }
