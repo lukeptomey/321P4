78b345292a1a1ec4fc0b410a4632a4072fc22be2
JeremyBouchard220 <60022660+JeremyBouchard220@users.noreply.github.com>
2020-05-05 12:30:35 -0700

    Update GeneBankSearch.java

--- GeneBankSearch.java
+++ GeneBankSearch.java
@@ -1,4 +1,15 @@
+import java.io.IOException;
 
-public class GeneBankSearch {
-
+public class GeneBankSearch 
+{
+    static int useCache, cacheSize;
+    static String bTreeFilename, queryFilename;
+    
+    public static void main(String[] args) throws IOException
+    {
+         useCache = Integer.parseInt(args[0]);
+         bTreeFilename = args[1];
+         queryFilename = args[2];
+         cacheSize = 0;
+    }
 }
