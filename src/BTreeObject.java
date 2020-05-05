915256f6bd1004bd722312a4f62a95cd6c402427
lukeptomey <lukeptomey@u.boisestate.edu>
2020-05-05 11:40:50 -0600

    delete src directory

--- src/BTreeObject.java
+++
@@ -1,8 +0,0 @@
-public class BTreeObject {
-    long dna;
-    int frequency;
-    public BTreeObject(long dna,int  frequency){
-        this.dna=dna;
-        this.frequency=frequency;
-    }
-    }
