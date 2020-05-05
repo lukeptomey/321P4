dd20a7024308b4896160983b8f1e59418c48ef1d
JeremyBouchard220 <60022660+JeremyBouchard220@users.noreply.github.com>
2020-05-05 14:23:27 -0700

    Insert and Insert Non Full Update
    Added portions of Insert and Insert Non Full for Luke to look at and verify.

--- BTree.java
+++ BTree.java
@@ -42,12 +42,21 @@ catch (IOException e){
 	/**
      * Inserts new node
      * @param BTree
-     * @param key to be inserted
+     * @param newKey to be inserted
      */
-    public void insert( Long key){
-
+    public void insert(Long key){
+    
+    //For @Luke to check
+    // for(int i = 0; i < root.getAmountOfKeys(); i++)
+    // {
+    //     if(key == root.getKeyAtIndex(i))
+    //     {
+    //         root.setKeyAtIndex(i, root.getKeyAtIndex(i) + 1);
+    //         file.writeNode(root);
+    //         return;
+    //     }
+    // }
     //check node if key exits, if so increase frequcy by one    
-
     if(root.getAmountOfKeys()== 2 *degree-1){
        newNode=file.createNode();
         file.setRootLocation(newNode.getLocation());
@@ -116,7 +125,19 @@ catch (IOException e){
      * @param key new key
      */
     public void insertNonFull(BTreeNode x, long key){
-     int i = x.getAmountOfKeys();
+     
+        //For @Luke to check
+        // for(int i = 0; i < root.getAmountOfKeys(); i++)
+        // {
+        //     if(key == root.getKeyAtIndex(i))
+        //     {
+        //         root.setKeyAtIndex(i, root.getKeyAtIndex(i) + 1);
+        //         file.writeNode(root);
+        //         return;
+        //     }
+        // }
+     
+        int i = x.getAmountOfKeys();
         if (x.checkLeaf()){ //x is a leaf and nonfull, insert k into x directly
          while(i>=0 && key < x.getKeyAtIndex(i).dna ){
              x.setKeyAtIndex(i+1, x.getKeyAtIndex(i));
